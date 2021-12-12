dab3d2517db200901075c50eb1844f798e6f3c3c
Cuong Nguyen <3936946+cuongptnk@users.noreply.github.com>
2021-12-12 09:41:37 -0400

    day12

---
+++ day12/part_2.py
@@ -0,0 +1,67 @@
+import queue
+
+
+def is_already_twice(path):
+    fre = {}
+    for v in path:
+        if v not in ['start', 'end'] and v.islower():
+            if v not in fre:
+                fre[v] = 1
+            else:
+                fre[v] += 1
+    for v in fre.values():
+        if v >= 2:
+            return True
+    return False
+
+
+def is_able_to_visit(path, n):
+    if n[0].isupper():
+        return True
+    if n in ['start', 'end'] and n in path:
+        return False
+    if n not in path:
+        return True
+    if is_already_twice(path):
+        return False
+    return True
+
+
+def solution(adj):
+    q = queue.Queue()
+    path = ['start']
+    q.put(path.copy())
+    count = 0
+    while not q.empty():
+        current_path = q.get()
+
+        for n in adj[current_path[-1]]:
+            if n == 'end':
+                count += 1
+                continue
+            if is_able_to_visit(current_path, n):
+                new_path = current_path.copy()
+                new_path.append(n)
+                q.put(new_path)
+    return count
+
+
+if __name__ == "__main__":
+    # input_file = 'input_sample.txt'
+    input_file = 'input_full.txt'
+    adj = {}
+    with open(input_file, 'r') as f:
+        for line in f:
+            line = line.strip().split('-')
+            if line[0] not in adj:
+                adj[line[0]] = [line[1]]
+            else:
+                adj[line[0]].append(line[1])
+            if line[1] not in adj:
+                adj[line[1]] = [line[0]]
+            else:
+                adj[line[1]].append(line[0])
+        print(solution(adj))
+
+
+
